9c312650c425fcaf13ed6688085626e2f70a0701
mateuszgil <mateusz.gil@ready4s.com>
2024-05-14 09:30:35 +0200

    add cli interface

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,71 @@
+use std::num::NonZeroI128;
+
+use clap::{builder::Str, Arg, Args, Parser, Subcommand};
+use kvs::KvStore;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get(GetArgs),
+    Set(SetArgs),
+    Remove(RemoveArgs)
+}
+
+#[derive(Args)]
+struct GetArgs {
+    key: Option<String>
+}
+
+#[derive(Args)]
+struct SetArgs {
+    key: Option<String>,
+    value: Option<String>
+}
+
+#[derive(Args)]
+struct RemoveArgs {
+    key: Option<String>
+}
+
+
 fn main() {
-    println!("hello world")
+    let cli = Cli::parse();
+
+    let mut kv_store = KvStore::new();
+
+    match &cli.command {
+        Commands::Get(key) => {
+             match &key.key {
+                 Some(val) => {
+                    kv_store.get(val.to_string());
+                },
+                 None => panic!("key is required")
+             }
+            
+        },
+        Commands::Set(args) => {
+            
+            let key = &args.key;
+            let value = &args.value;
+            if let (Some(key), Some(value)) = (key, value) {
+                kv_store.set(key.to_string(), value.to_string())
+            } else {
+                panic!("key and value are required")
+            }
+        },
+        Commands::Remove(key) => {
+            match &key.key {
+                Some(val) => {
+                    kv_store.remove(val.to_string())
+                },
+                None => panic!("key is required")
+            }
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 pub struct KvStore {}
 
 impl KvStore {
-    pub fn new () -> KvStore {
-        KvStore {}
+    pub fn new () -> Self {
+        Self {}
+    }
+
+    pub fn set (&mut self, _key: String, _val: String) {
+        panic!("unimplemented")
+    }
+
+    pub fn get(&mut self, _key: String) -> Option<String> {
+        panic!("unimplemented")
+    }
+
+    pub fn remove(&mut self, _key: String) {
+        panic!("unimplemented")
     }
 }
